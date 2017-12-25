0c522c8a94fb3eb3abcbd38baa789e922e151c51
rodneystrong <akang2@gmail.com>
2017-12-24 17:32:35 -0800

    refactored into functions outside of loop

--- app.js
+++ app.js
@@ -5,22 +5,34 @@ var input = prompt("what you wanna do");
 //if input is 'list' then list all the Todos
 while (input !== "quit") {
   if (input === "list") {
-    console.log("********");
-    todos.forEach(function(item, index) {
-      console.log(index + ": " + item);
-    });
-    console.log("********");
+    listItems();
   } else if (input === "new") {
-    var newItem = prompt("gimme the todo");
-    todos.push(newItem);
-    console.log(todos);
+    addItem();
   } else if (input === "delete") {
-    //ask for the index of the item
-    var delItem = prompt("what index wanna delete");
-    todos.splice(delItem, 1);
-    console.log("you removed " + delItem);
+    delItem();
   }
   input = prompt("what you wanna do");
 }
 
 console.log("you quitter");
+
+function listItems() {
+  console.log("********");
+  todos.forEach(function(item, index) {
+    console.log(index + ": " + item);
+  });
+  console.log("********");
+}
+
+function addItem() {
+  var newItem = prompt("gimme the todo");
+  todos.push(newItem);
+  console.log("added " + newItem);
+}
+
+function delItem() {
+  //ask for the index of the item
+  var deleted = prompt("what index wanna delete");
+  todos.splice(deleted, 1);
+  console.log("you removed " + deleted);
+}

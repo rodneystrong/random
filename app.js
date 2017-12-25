1daec3fdc70f2ae5d0d62ce5b464ba67c2174fd7
rodneystrong <akang2@gmail.com>
2017-12-24 17:00:35 -0800

    add new item from prompt

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ var input = prompt("what you wanna do");
 //if input is 'list' then list all the Todos
 if (input === "list") {
   console.log(todos);
+} else if (input === "new") {
+  var newItem = prompt("gimme the todo");
+  todos.push(newItem);
 }

1b60f8beb1c91d440f7957aedff93716f35a82d9
rodneystrong <akang2@gmail.com>
2017-12-24 17:26:30 -0800

    logic for deleting items

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ var input = prompt("what you wanna do");
 //if input is 'list' then list all the Todos
 while (input !== "quit") {
   if (input === "list") {
-    todos.forEach(function(item) {
-      console.log(item);
+    console.log("********");
+    todos.forEach(function(item, index) {
+      console.log(index + ": " + item);
     });
+    console.log("********");
   } else if (input === "new") {
     var newItem = prompt("gimme the todo");
     todos.push(newItem);
     console.log(todos);
+  } else if (input === "delete") {
+    //ask for the index of the item
+    var delItem = prompt("what index wanna delete");
+    todos.splice(delItem, 1);
+    console.log("you removed " + delItem);
   }
   input = prompt("what you wanna do");
 }

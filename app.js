753fef049ca97d8cf4e3bb8fd951f77798dadea1
rodneystrong <akang2@gmail.com>
2017-12-24 17:07:39 -0800

    added logic for looping until exit condition occurs

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ var todos = ["get tall", "get smart", "get happy"];
 var input = prompt("what you wanna do");
 
 //if input is 'list' then list all the Todos
-if (input === "list") {
-  console.log(todos);
-} else if (input === "new") {
-  var newItem = prompt("gimme the todo");
-  todos.push(newItem);
+while (input !== "quit") {
+  if (input === "list") {
+    console.log(todos);
+  } else if (input === "new") {
+    var newItem = prompt("gimme the todo");
+    todos.push(newItem);
+    console.log(todos);
+  }
+  input = prompt("what you wanna do");
 }
+
+console.log("you quitter");
